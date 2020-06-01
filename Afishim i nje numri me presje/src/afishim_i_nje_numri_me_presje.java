2419b183031ecc628b76106bb1572fbd8061813c
disar_000 <disaracuraj@hotmail.com>
2020-06-01 18:18:07 +0100

    Afishim i nje numri me presje

---
+++ Afishim i nje numri me presje/src/afishim_i_nje_numri_me_presje.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class afishim_i_nje_numri_me_presje {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		double d=5.1234678;
+		System.out.printf("%.2f %n",d);
+
+	}
+
+}
